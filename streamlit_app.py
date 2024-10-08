12c8dd9315620f71631eb96116b18205ada3384c
DoggieRover <140857968+DoggieRover@users.noreply.github.com>
2024-10-08 21:46:35 +0000

    Learn01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Doggie Rover is Golden")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Rovers App  head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
